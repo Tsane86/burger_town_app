35f3f5b4318b91c9b261f10767e2eade6eb24d67
Brenton <brenton@breakdesign.com.au>
2024-03-26 16:45:37 +1100

    Data validation complete

--- burger.py
+++ burger.py
@@ -1,19 +1,24 @@
 # class here for object creation
 class Burger:
-    def __init__(self, type_of_bun, sauce, cost):
+    def __init__(self, type_of_bun):
         self.bun = type_of_bun
-        self.sauce = sauce
-        self.cost = cost
+        #self.sauce = sauce
+        #self.cost = cost
     
     def __str__(self):
-        return f'Type of Bun: {self.bun}, Cost: {self.cost}' # extend this out
+        return f'Type of Bun: {self.bun}' # extend this out
     
 # data validation function. Will check for if the entries is one of the options for the input
-def check_input(input_list, input_options):
-    for input in input_list:
-        return input.lower() in input_options #this will return true or false, which can then be used in a conditional
+def check_input(input_options, input_question):
+    input_choice = input(input_question)
+    if input_choice.lower() in input_options:
+        return input_choice
+    else:
+        print('Not a valid option')
+        check_input(input_options, input_question) # recursion means this will loop until valid option chosen
 
-input_options =['milk','gluten free','tomato','bbq']
+bun_options = ['milk','gluten free']
+sauce_options = ['tomato', 'barbecue', 'none']
 
 # costs for burger
 plain_burger = 5
@@ -31,16 +36,14 @@ def menu():
         burger_number = int(input('How many burgers would you like to order [1-10] \n'))
         if burger_number > 0 and burger_number <= 10:
             for number in range(burger_number):
-                type_of_bun = input(f'What bun type should be included for Burger {number + 1}? Milk or Gluten Free? \n')
-                
-                sauce = input(f'What sauce should be included on Burger {number}? Tomato, BBQ or None? \n')
-                if check_input([type_of_bun, sauce], input_options) != True:
-                    print('Please double check your entry')
-                    menu()
+                type_of_bun = check_input(bun_options, input_question=(f"What bun type should be included for Burger {number + 1}? {bun_options}? "))
+                sauce = check_input(sauce_options, input_question=(f"What sauce should be included on Burger {number + 1}? {sauce_options}? "))
+                #patties = check_input(patties_options, input_question=(f"How many patties should be on Burger {number + 1} [0-3]"))
+                #slices_of_cheese = check_input(cheese_options, input_question=(f"How many slices of cheese should be on Burger {number + 1} [0-3]"))
                 cost = 5
                 if type_of_bun.lower() == 'gluten free':
                     cost += 1
-            return burger_array.append(Burger(type_of_bun=type_of_bun, sauce=sauce, cost=cost))
+            return burger_array.append(Burger(type_of_bun=type_of_bun))
         else:
             print('Please enter a number between 1 and 10')
             menu()
