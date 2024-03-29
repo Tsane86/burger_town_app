c52f130936520cb5a6ea211b666d2ffe78d203a5
Brenton <brenton@breakdesign.com.au>
2024-03-29 13:20:01 +1100

    updated with cost functions

--- burger.py
+++ burger.py
@@ -13,7 +13,7 @@ class Burger:
     def __str__(self):
         return f'Type of Bun: {self.bun}, Sauce: {self.sauce}, Patties: {self.patties}, Cheese: {self.cheese}, Tomato: {self.toamto}, Lettuce: {self.lettuce}, Onion: {self.onion}, Cost: {self.cost}' # extend this out
     
-# data validation function. Will check for if the entries is one of the options for the input
+# data validation function. Will check for if the entries is one of the options for the input question.
 def check_input(input_options, input_question):
     input_choice = input(input_question)
     if input_choice.lower() in input_options:
@@ -22,11 +22,30 @@ def check_input(input_options, input_question):
         print(f'Not a valid option. Please enter a valid option: {input_options}')
         check_input(input_options, input_question) # recursion means this will loop until valid option chosen
 
+def calculate_burger_cost(type_of_bun, patties, slices_of_cheese, has_lettuce, has_onion, has_tomato):
+    cost = 5
+    if type_of_bun.lower() == 'gluten free':
+        cost += 1
+    if patties > 1:
+        cost += (3*(patties-1))
+    if slices_of_cheese > 1:
+        cost += (1*(slices_of_cheese-1))
+    salad_options = [has_lettuce, has_onion, has_tomato]
+    number_of_salad_items = 0
+    for salad in salad_options:
+        if salad == 'yes':
+            number_of_salad_items += 1
+    if number_of_salad_items > 1:
+       cost += (1*(number_of_salad_items-1)) 
+    return cost #TODO test this
+        
+
 # options for user input
 bun_options = ['milk', 'gluten free']
 sauce_options = ['tomato', 'barbecue', 'none']
 patties_options = cheese_options = ['0', '1', '2' , '3']
-tomato_options = ['yes','no']
+tomato_options = lettuce_options = onion_options = ['yes','no']
+#TODO add more options
 
 # costs for burger
 plain_burger = 5
@@ -43,6 +62,7 @@ def calculate_total_cost(burger_array):
     for burger in burger_array:
         total_cost += burger.cost
     return total_cost
+#TODO check this works
 
 # Menu
 def menu():
@@ -54,12 +74,15 @@ def menu():
                 type_of_bun = check_input(bun_options, input_question=(f"What bun type should be included for Burger {number + 1}? {bun_options}? "))
                 sauce = check_input(sauce_options, input_question=(f"What sauce should be included on Burger {number + 1}? {sauce_options}? "))
                 patties = check_input(patties_options, input_question=(f"How many patties should be on Burger {number + 1} [0-3]? "))
-                slices_of_cheese = check_input(cheese_options, input_question=(f"How many slices of cheese should be on Burger {number + 1} [0-3]? "))\
-                # start here
+                slices_of_cheese = check_input(cheese_options, input_question=(f"How many slices of cheese should be on Burger {number + 1} [0-3]? "))
+                has_tomato = check_input(tomato_options, input_question=(f"Should Burger {number + 1} have tomato {tomato_options}? "))
+                has_lettuce = check_input(lettuce_options, input_question=(f"Should Burger {number + 1} have lettuce {lettuce_options}? "))
+                has_onion = check_input(onion_options, input_question=(f"Should Burger {number + 1} have onion {onion_options}? "))
                 cost = 5
-                if type_of_bun.lower() == 'gluten free':
-                    cost += 1
-            return burger_array.append(Burger(type_of_bun=type_of_bun))
+                #TODO cost calculator, might be easier as a function. input will need to be type of bun,
+
+                
+            return burger_array.append(Burger(type_of_bun=type_of_bun, sauce=sauce, patties=patties, cheese=slices_of_cheese, tomato=has_tomato, lettuce=has_lettuce, onion=has_onion, cost=cost))
         else:
             print('Please enter a number between 1 and 10')
             menu()
@@ -70,8 +93,7 @@ def menu():
 
 def start_app():
     menu()
-    for burger in burger_array:
-        print(burger)
+    print(burger_array)
 
 if __name__ == '__main__':
     start_app()
